853fa483836c8fdc3cd98d4cba0f83ffb77a6dd8
Iyadh <yad.juini@gmail.com>
2023-08-30 19:18:05 +0200

    Creat_React_App

---
+++ Day-03/Core/create-react-app/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World !</h1>
+      <h5>Things I Need to Do</h5>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the Dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
