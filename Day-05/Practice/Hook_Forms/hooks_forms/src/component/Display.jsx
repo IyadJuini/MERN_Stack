3d6a2bb1dc3a88afcae7c23bcae1c71c42d1dccc
Iyadh <yad.juini@gmail.com>
2023-09-01 19:42:02 +0200

    Hooks Forms

---
+++ Day-05/Practice/Hook_Forms/hooks_forms/src/component/Display.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Display = (props) => {
+    return (
+        <fieldset>
+            <legend><h1>👻👻👻👻Display👻👻👻👻</h1></legend>
+            <p>First Name: {props.firstName}</p>
+            <p>Last Name: {props.lastName}</p>
+            <p>Email: {props.email}</p>
+            <p>Password: {props.password}</p>
+        </fieldset>
+
+    )
+}
+
+export default Display
